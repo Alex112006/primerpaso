b74849f3273c765afbb312aa6c3acbff3a098a02
Alex112006 <alexjhosepccoyllos@gmail.com>
2025-09-16 21:50:30 -0500

    Create appy2.py

---
+++ appy2.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title("Titulo Deisy")
+num=st.slider("num", 0, 100, step=1)
+st.write("el numero ingresado es {}".format(num))
+
+guárdalo en GitHub como appy2.py
